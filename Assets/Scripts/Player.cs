710d621c8179b5700b9a08d3dc7fa2974d1d9409
Simona <sima.simute1999@gmail.com>
2024-12-02 17:38:48 +0200

    Coyote time & Jump buffer

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -13,7 +14,14 @@ public class Player : MonoBehaviour
     public Transform groundCheck;//character legs
     public float radius = 0.2f;
     public LayerMask groundMask;
+
+    [Header("Jump Mechanics")]
+    public float coyoteTime = 0.2f;
+    public float jumpBufferTime = 0.2f;
     
+
+    private float coyoteBuffer;
+    private float jumpBuffer;
     private Rigidbody2D rb;
     private float horizontal;
     private bool isGrounded;
@@ -30,8 +38,29 @@ public class Player : MonoBehaviour
         
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, radius, groundMask);
         
-        if (isGrounded && Input.GetButtonDown("Jump") )
+        if(isGrounded)
         {
+            coyoteBuffer = coyoteTime;
+        }
+        else
+        {
+            coyoteBuffer -= Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpBuffer = jumpBufferTime;
+        }
+        else
+        {
+            jumpBuffer -= Time.deltaTime;
+        }
+
+
+        if (jumpBuffer > 0 && coyoteBuffer > 0)
+        {
+            jumpBuffer = 0;
+
             var jumpForce = Mathf.Sqrt(jumpHeight * -2f * Physics.gravity.y * rb.gravityScale);
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
@@ -50,5 +79,15 @@ public class Player : MonoBehaviour
             Gizmos.DrawWireSphere(groundCheck.position, radius);
         }
         
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //apply fall damage
+        if(collision.relativeVelocity.y > 25)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
